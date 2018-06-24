4cdc73943bc7a72bd2ddf02bf5b172b144b69dd5
dshahid380 <36432967+dshahid380@users.noreply.github.com>
2018-06-24 21:57:19 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Keywords" content="Md Shahid,md shahid,MD SHAHID,MD Shahid,dshahid380,DSHAHID380,DSHAHID,dshahid,Shahid,Shahid khan,Shahid Afridi,github/dshahid380,linkenin/dshahid380,Coder,programmer,Programmer,deep learning geek,Full stack developer">
+    <meta name="Description" content="This is a portfolio website">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+    <title>Md Shahid</title>
+</head>
+
+     
+<body>
+    <!--Nav bar start-->
+    <div class="navbar-fixed">
+
+        <nav class="green accent-3">
+             
+            <div class="container">
+                <div class="nav-wrapper">
+                    
+                    <a href="#" class="brand-logo logo">&lt;/&gt;</a>
+                    <a href="#" class="right hide-on-small-only" id="show_time"></a>
+                    <a href="" data-target="mobile-nav" class="sidenav-trigger">
+                        <i class="material-icons">menu</i>
+                    </a>
+                    <ul class="right hide-on-med-and-down">
+                         <li>
+                            <a href="index.html"><i class="fa fa-home"></i> <span class="white-text">Home </span></a>
+                        </li>
+                         <li>
+                            <a  href='https://github.com/dshahid380'><span class="white-text">Projects </span> </i></a>
+                        </li>
+                        <li>
+                            <a href="https://docs.google.com/forms/d/1y33l4u1mRnGRizi1JWEn7eXYYst8RWwkrFmGbl7eRnY/edit"><i class="fa fa-comments"></i> <span class="white-text">Contact </span></a>
+                        </li>
+                         
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <ul class="sidenav green accent-3" id="mobile-nav">
+        
+            <li>
+               <a href="index.html"><i class="fa fa-home"></i> <span class="white-text">Home </span></a>
+           </li>
+            <li>
+               <a  href='https://github.com/dshahid380'><i class="fa fa-suitcase"></i><span class="white-text">Projects </span> </i></a>
+           </li>
+           <li>
+               <a href="https://docs.google.com/forms/d/1y33l4u1mRnGRizi1JWEn7eXYYst8RWwkrFmGbl7eRnY/edit"><i class="fa fa-comments"></i> <span class="white-text">Contact </span></a>
+           </li>
+            
+       
+    </ul>
+</div>
+    <!--Nav bar End-->
+     
+     <section >
+          <div class="container">
+              <div style="background:#f3e5f5;" class="row card-panel">
+                  <div class="col s12 m4">
+                      <img  class="responsive-img profile_img" src="img/img1.jpeg" alt="">
+                  </div>
+                  <div class="col s12 m8 info">
+                      <h5>Hi,</h5><h3> <span class="red">   I'm</span><span class="orange"> Md </span> <span class="green accent-3"> Shahid</span></h3>
+                      <h5><strong class="teal-text"> Fullstack Web developer || Deep Learning geek </strong></h5>
+                      <strong class="purple-text">( Web developer at Tourlancers  &  Campus Ambassador at GeeksforGeeks.org )</strong><br>
+                      
+                  </div>
+              </div>
+              <br>
+               <div class="row">
+                  <div class="row">
+                     
+                    <div class="col center s12 green accent-3">
+                        <h5><i class="fa fa-book-open"></i> SKILL</h5>
+                    </div>
+                    <div class="row center purple lighten-5 skill">
+                        <table class="centered striped">
+                            <tbody>
+                              <tr>
+                                <td>HTML</td>
+                                <td>CSS</td>
+                                
+                              </tr>
+                              <tr>
+                                <td>Javascript</td>
+                                <td>jQuery</td>
+                                
+                              </tr>
+                              <tr>
+                                <td>Bootstap</td>
+                                <td>Materialize CSS</td>
+                                
+                              </tr>
+                              <tr>
+                                <td>C</td>
+                                <td>C++</td>
+                                
+                              </tr>
+                              <tr>
+                                <td>Java</td>
+                                <td>Python</td>
+                                
+                              </tr>
+                              <tr>
+                                <td>Kotlin</td>
+                                <td>PHP</td>
+                                
+                              </tr>
+                              <tr>
+                                <td>SEO</td>
+                                <td>MySQL</td>
+                                
+                              </tr>
+                              <tr>
+                                <td>SQL</td>
+                                <td>Django</td>
+                              </tr>
+                              <tr>
+                                <td>AJAX</td>
+                                <td>SASS</td>
+                              </tr>
+                            </tbody>
+                          </table>      
+                    </div>
+                
+                  </div>
+                    <section>
+                    <div class="col center s12 green accent-3">
+                        <h5> <i class="fas fa-bookmark"></i> CERTIFICATIONS</h5>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h5>Python, data structure and algorithm </h5>
+                                 <b>-NPTEL</b>
+                            </div>
+                        </div>
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h5>Internet of things </h5>
+                                 <b>-Internshala</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h5>Quadcopter workshop </h5>
+                                 <b>-mywbut.com</b>
+                            </div>
+                        </div>
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h5>Ethical hacking </h5>
+                                 <b>-mywbut.com</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h5>Javascript </h5>
+                                 <b>-Udemy</b>
+                            </div>
+                        </div>
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h5>ECMAScript </h5>
+                                 <b>-Udemy</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h5>Bootstrap </h5>
+                                 <b>-learncodeonline.con</b>
+                            </div>
+                        </div>
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h5>Java </h5>
+                                 <b>-Udemy</b>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div class="col center s12 green accent-3">
+                        <h5><i class="fas fa-graduation-cap"></i> EDUCATIONS</h5>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h6><i class="fas fa-school"></i>PRIMARY SCHOOL</h6>
+                                 <b>Shiksha Niketan</b>
+                                 <p>Chitrigung Budge Budge Kol-700137 </p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m6 offset-m6">
+                            <div class="card-panel purple lighten-5">
+                                <h6><i class="fas fa-school"></i> SCHOOL</h6>
+                                 <b>Arya Parishad Vidyalaya</b>
+                                 <p> 1 Nimak mahal Road,, Circular Garden Reach Road, Garden Reach, Kolkata, West Bengal 700043</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m6">
+                            <div class="card-panel purple lighten-5">
+                                <h6><i class="fas fa-school"></i>HIGHER SECONDARY</h6>
+                                 <b>Arya Parishad Vidyalaya</b>
+                                 <p> 1 Nimak mahal Road,, Circular Garden Reach Road, Garden Reach, Kolkata, West Bengal 700043</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m6 offset-m6">
+                            <div class="card-panel purple lighten-5">
+                                <h6><i class="fas fa-school "></i> COLLEGE</h6>
+                                 <b>Netaji Subhash Engineering College</b>
+                                 <p>Panchpota, Garia, Kol-700152</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </section> 
+             </div>
+          </div>
+     </section>
+     <section class="section section-follow green accent-3 white-text center">
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <h4>Follow me</h4>
+                    <p>Follow me on social media for special offers</p>
+                    <a href="" class="white-text">
+                        <i class="fab fa-facebook fa-4x"></i>
+                    </a>
+                    <a href="https://github.com/dshahid380" target="_blank" class="white-text">
+                          <i class="fab fa-github fa-4x"></i>
+                    </a>
+                      <a href="https://in.linkedin.com/in/dshahid380?trk=profile-badge" target="_blank"  class="white-text">
+                              <i class="fab fa-linkedin fa-4x"></i>
+                      </a>
+                      <a href="mailto:dshahid3805@gmail.com" class="white-text">
+                              <i class="fab fa-google-plus fa-4x"></i>
+                      </a>
+                      
+                </div>
+            </div>
+        </div>   
+       
+       </section>
+      <!--Social Media follow End-->
+       
+        <!--footer-->
+     <footer class="section green accent-3 white-text center">
+        <p class="flow-text">Md Shahid &copy;2017-2018</p>
+    </footer>
+   <!--footer end-->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+     <script>
+       $(document).ready(function(){
+       $('.sidenav').sidenav();});   
+         
+     </script>
+</body>
+</html>
